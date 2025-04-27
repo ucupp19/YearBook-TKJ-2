45f1acca1c7fca93307bf6f3130fec3b28649bb6
Afrizz <quotesindonesia277@gmail.com>
2025-04-27 14:32:57 +0700

    image

--- client/src/data/galleryImages.ts
+++ client/src/data/galleryImages.ts
@@ -2,75 +2,74 @@ export const galleryImages = [
   // First row
   {
     url: new URL('../data/gambar/FOTO1.jpg', import.meta.url).href,
-    alt: 'Students collaborating',
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO2.png', import.meta.url).href,
-    alt: "Computer lab"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO3.png', import.meta.url).href,
-    alt: "Networking equipment"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO4.png', import.meta.url).href,
-    alt: "Group project"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO5.png', import.meta.url).href,
-    alt: "School event"
+    alt: 'IDK',
   },
-  {
-    url: new URL('../data/gambar/FOTO12.png', import.meta.url).href,
-    alt: "Graday"
-  },
-  // Second row
   {
     url: new URL('../data/gambar/FOTO6.png', import.meta.url).href,
-    alt: "Network setup"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO7.png', import.meta.url).href,
-    alt: "Student coding"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO8.png', import.meta.url).href,
-    alt: "Field trip"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO9.png', import.meta.url).href,
-    alt: "Classroom activity"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO10.png', import.meta.url).href,
-    alt: "Grad"
+    alt: 'IDK',
   },
   {
     url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO12.png', import.meta.url).href,
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO13.png', import.meta.url).href,
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO14.png', import.meta.url).href,
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO15.png', import.meta.url).href,
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO16.png', import.meta.url).href,
+    alt: 'IDK',
   },
   {
-    url: new URL('../data/gambar/FOTO11.png', import.meta.url).href,
-    alt: "Gra"
+    url: new URL('../data/gambar/FOTO17.png', import.meta.url).href,
+    alt: 'IDK',
+  },
+  {
+    url: new URL('../data/gambar/FOTO18.png', import.meta.url).href,
+    alt: 'IDK',
   },
 ];
