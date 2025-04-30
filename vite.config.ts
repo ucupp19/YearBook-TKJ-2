f43a6c963d3fc14b1acbcb44c08ca07de029ae66
Afrizz <quotesindonesia277@gmail.com>
2025-04-30 18:59:15 +0700

    chanegr

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,6 @@ import path from "path";
 
 
 export default defineConfig({
-  plugins: [react(),],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
-    },
+  plugins: [react(),],  
   },
-  root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-});
+);
